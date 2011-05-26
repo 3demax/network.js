2e3c9c47d918fb9fc5895f34c6a13356b2b1931b
Andy Snagovsky <dv5ife@gmail.com>
2011-05-26 18:17:54 +0300

    Fixed request sending

--- network.js
+++ network.js
@@ -1,22 +1,33 @@
+/*http = new XMLHttpRequest();
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+http.setRequestHeader("Content-length", params.length);
+http.setRequestHeader("Connection", "close");
+*/
 network = {
 	url: "/api/",
 	_send : function(parameters){
 		this.xhr = new Xhr(this.url, {
+		    urlEncoded: false,
 			onSuccess: function(request){
 				console.log(request.responseText);				
 				this.receiveCallback(JSON.parse(request.responseText));
 			},
-		}).send(parameters);
+		}).send(JSON.stringify(parameters));
 		this.xhr.receiveCallback = this.receiveCallback;
-	},
+	}, 
+
+    /*_send: function(params){
+	http.open('POST', this.url, true);
+	http.send(params);
+    },*/
 	onRecieve : function(callback){
 		this.receiveCallback = callback;
 	},
 	send: function(actionName, args){
 		if ((typeof args == "undefined") || (args == "")) args = null;
 		this._send({
-			name: actionName,
-			params: args,
+		    "Action": {"name":actionName, "params":args},
+		    "AppID":1
 		})
 	}
 }
